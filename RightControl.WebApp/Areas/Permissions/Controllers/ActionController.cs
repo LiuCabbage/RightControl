e09d3ba5c6be52a2890c738dffee09a812f2c74a
Liu_Cabbage <178899573@qq.com>
2019-06-11 17:41:50 +0800

    RightControl

--- RightControl.WebApp/Areas/Permissions/Controllers/ActionController.cs
+++ RightControl.WebApp/Areas/Permissions/Controllers/ActionController.cs
@@ -30,10 +30,45 @@ namespace RightControl.WebApp.Areas.Permissions.Controllers
             var result = service.GetListByFilter(filter, pageInfo);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-        public ActionResult Detail(int id)
+        public ActionResult Detail(int Id)
         {
-            var model = service.ReadModel(id);
+            var model = service.ReadModel(Id);
             return View(model);
         }
+        public ActionResult Edit(int Id)
+        {
+            ViewData["Position"] = EnumExt.GetSelectList(typeof(PositionEnum));
+            var model = service.ReadModel(Id);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(ActionModel model)
+        {
+            model.UpdateOn = DateTime.Now;
+            model.UpdateBy = Operator.UserId;
+            var result = service.UpdateModel(model) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+        public ActionResult Add()
+        {
+            ViewData["Position"] = EnumExt.GetSelectList(typeof(PositionEnum));
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(ActionModel model)
+        {
+            model.CreateOn = DateTime.Now;
+            model.CreateBy = Operator.UserId;
+            var result = service.CreateModel(model) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+        [HttpPost]
+        public ActionResult Delete(int Id)
+        {
+            var result = service.DeleteModel(Id) ? SuccessTip() : ErrorTip();
+            return Json(result);
+        }
+
+
     }
 }
